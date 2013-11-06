d32022957f2414e6d3b61e5745085ac193ff59e3
Marius Mandal <mariusmandal@gmail.com>
2013-11-06 10:47:21 +0100

    test label

--- config.php
+++ config.php
@@ -7,11 +7,14 @@ $TV->videofile();
 
 if($TV->ext == '.mp4') {
 	$sources = 'sources: [{
-				    file: "rtmp://212.125.231.33/ukmtv/_definst_/mp4:'.$TV->file.'"
+				    file: "rtmp://212.125.231.33/ukmtv/_definst_/mp4:'.$TV->file.'",
+				    label: "720p"
 				},{
-				    file: "http://212.125.231.33/ukmtv/_definst_/'.$TV->file.'/playlist.m3u8"
+				    file: "http://212.125.231.33/ukmtv/_definst_/'.$TV->file.'/playlist.m3u8",
+				    label: "720p"
 				},{
-				    file: "http://video.ukm.no/'.$TV->file.'"
+				    file: "http://video.ukm.no/'.$TV->file.'",
+				    label: "720p"
 				}]';
 } else {
 	$sources = 'file: "http://video.ukm.no/'.$TV->file.'"';
