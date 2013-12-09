9b0858410b435a39aa81a7afa54567a5ac8dfe4f
Marius Mandal <mariusmandal@gmail.com>
2013-12-09 13:30:13 +0100

    Google analytics

--- index.php
+++ index.php
@@ -30,17 +30,17 @@ if($TV->id) { ?>
 	<script language="javascript" src="http://embed.ukm.no/info/<?= $_GET['f'] ?><?= isset($_GET['autoplay']) || isset($_GET['autostart']) ? '?autoplay=true': ''?>"></script>
 <?php } ?>
 <script type="text/javascript">
-
+ 
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-27157707-11']);
+  _gaq.push(['_setAccount', 'UA-46216680-7']);
   _gaq.push(['_trackPageview']);
-
+ 
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
+ 
 </script>
 </head>
 <body>
