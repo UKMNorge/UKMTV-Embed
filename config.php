9fab416b889cbda97037d3f33f78f17e0cbc6585
jardarn <jardar@ukm.no>
2014-04-07 13:18:12 +0200

    Update config.php
    fix

--- config.php
+++ config.php
@@ -9,8 +9,7 @@ $TV->videofile();
 $storageIP = 'storageIP'.$TV->activeStorage;
 $storageURL= 'storageurl'. $TV->activeStorage;
 
-if($TV->ext == '.mp4') {
-	else if($TV->activeStorage  == '2') {
+if($TV->ext == '.mp4' && $TV->activeStorage  == '2') {
 		$sources = 'sources: [{
 					    file: "rtmp://'.$TV->$storageIP.'/ukmtv/_definst_/mp4:'.str_replace('720p','mobile', $TV->file).'"
 					},{
@@ -18,7 +17,7 @@ if($TV->ext == '.mp4') {
 					},{
 					    file: "'.$TV->$storageURL.str_replace('720p','mobile', $TV->file).'"
 					}]';
-	} else if($TV->activeStorage  != '2') {
+	} else if($TV->ext == '.mp4') {
 		$sources = 'sources: [{
 					    file: "rtmp://'.$TV->$storageIP.'/ukmtv/_definst_/mp4:'.$TV->file.'"
 					},{
