3e0a4c823821cb1e108a7c440def9915d31a1ef4
jardarn <jardar@ukm.no>
2014-04-16 23:15:23 +0200

    Update config.php
    smil remove rtmp

--- config.php
+++ config.php
@@ -11,8 +11,6 @@ $storageURL= 'storageurl'. $TV->activeStorage;
 
 if($TV->ext == '.mp4' && $TV->activeStorage == '2') {
 	$sources = 'sources: [{
-				file: "rtmp://'.$TV->$storageIP.'/ukmtv/_definst_/mp4:'.$TV->file.'"
-				},{
 				file: "http://'.$TV->$storageIP.'/ukmtv/_definst_/smil:'.str_replace('_720p.mp4','.smil', $TV->file).'/playlist.m3u8"
 				},{
 				file: "'.$TV->$storageURL.$TV->file.'"
