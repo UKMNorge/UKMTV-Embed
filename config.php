70e778ca3141042b462d2315b6efa88a81d1150b
jardarn <jardar@ukm.no>
2014-04-20 19:52:00 +0200

    Update config.php
    sources

--- config.php
+++ config.php
@@ -13,6 +13,8 @@ if($TV->ext == '.mp4' && $TV->activeStorage == '2') {
 	$sources = 'sources: [{
 				file: "http://'.$TV->$storageIP.'/ukmtv/_definst_/smil:'.str_replace('_720p.mp4','.smil', $TV->file).'/playlist.m3u8"
 				},{
+				file: "rtmp:://'.$TV->$storageIP.'/ukmtv/_definst_/mp4:'.$TV->file.'"
+				},{
 				file: "'.$TV->$storageURL.$TV->file.'"
 				}]';
 } else if($TV->ext == '.mp4') {
